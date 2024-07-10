ab09c004ca5a6afe04ed777970ef35234d31d642
Abhinav Singh <abhinavsing@google.com>
2024-07-10 12:05:53 -0700

    Add checkpoint breakdown for Goodput measurement.
    PiperOrigin-RevId: 651106862

---
+++ ml_goodput_measurement/src/checkpoint_breakdown.py
@@ -0,0 +1,91 @@
+"""Checkpoint breakdown class."""
+
+import dataclasses
+from typing import Dict, List, Optional
+
+import google.cloud.logging as google_cloud_logging
+
+
+_JOB_NAME = 'checkpoint_job'
+_LOGGER_NAME = 'checkpoint_logger'
+
+_STEP = 'step'
+_BLOCKING_SAVE_START_TIME = 'checkpoint_start_time'
+_BLOCKING_SAVE_END_TIME = 'checkpoint_end_time'
+_EVENT_TYPE = 'event_type'
+_SYNCHRONOUS = 'synchronous'
+_PREEMPTION = 'reached_preemption'
+_PREEMPTION_RECEIVED_AT = 'preemption_received_at'
+_WAIT_FOR_PREV_START_TIME = 'wait_for_prev_start_time'
+_WAIT_FOR_PREV_END_TIME = 'wait_for_prev_end_time'
+
+
+# Total checkpoint save time.
+_TOTAL_SAVE_START_TIME = 'start_time'
+_TOTAL_SAVE_END_TIME = 'end_time'
+
+
+_CLOUD_LOGGING_PAGE_SIZE = 10000
+
+
+@dataclasses.dataclass
+class CheckpointLoggerOptions:
+  """Breakdown options."""
+  job_name: str = _JOB_NAME
+  logger_name: str = _LOGGER_NAME
+  client: Optional[google_cloud_logging.Client] = None
+
+
+class CheckpointBadputCalculator:
+  """Checkpoint breakdown class."""
+
+  def __init__(
+      self, options: CheckpointLoggerOptions = CheckpointLoggerOptions()
+  ):
+    self._options = options
+
+    if options.client is None:
+      self.logging_client = google_cloud_logging.Client()
+    else:
+      self.logging_client = options.client
+    self._logger = self.logging_client.logger(options.logger_name)
+    self.entries = []
+
+  def read_entries(self) -> List[Dict[str, str]]:
+    """Queries Cloud Logging entries for the specific job.
+
+    Returns:
+      Filtered entries in ascending order of timestamp.
+
+    """
+
+    filter_entries = [
+        'severity=INFO',
+        f'jsonPayload.job_name="{self._options.job_name}"',
+        'jsonPayload.event_type=save',
+    ]
+    filter_entries = ' AND '.join(filter_entries)
+    entries = self._logger.list_entries(
+        filter_=filter_entries,
+        order_by=google_cloud_logging.ASCENDING,
+        page_size=_CLOUD_LOGGING_PAGE_SIZE,
+    )
+    entry_payload = [entry.payload for entry in entries]
+    return entry_payload
+
+  def calculate_blocking_checkpoint_time(self) -> float:
+    """Gets blocking checkpoint time duration for total checkpoint."""
+    if not self.entries:
+      self.entries = self.read_entries()
+
+    total_blocking_checkpoint_time = 0.0
+    for step in self.entries:
+      if step[_PREEMPTION]:
+        total_blocking_checkpoint_time += float(
+            step[_TOTAL_SAVE_END_TIME]
+        ) - float(step[_WAIT_FOR_PREV_END_TIME])
+      else:
+        total_blocking_checkpoint_time += float(
+            step[_BLOCKING_SAVE_END_TIME]
+        ) - float(step[_BLOCKING_SAVE_START_TIME])
+    return total_blocking_checkpoint_time
---
+++ ml_goodput_measurement/tests/checkpoint_breakdown_test.py
@@ -0,0 +1,121 @@
+"""Tests for checkpoint breakdown."""
+
+import dataclasses
+from typing import Optional
+
+from absl.testing import absltest
+from cloud_tpu_goodput.ml_goodput_measurement.src import checkpoint_breakdown
+import google.cloud.logging as google_cloud_logging
+import mock
+
+
+_JOB_NAME = 'checkpoint_job'
+_LOGGER_NAME = 'checkpoint_logger'
+
+
+@dataclasses.dataclass
+class MockStepStatistics:
+  """Attributes.
+
+  Attributes:
+    step: The step number.
+    event_type: The event type.
+    start_time: The start time of the event.
+    end_time: The end time of the event.
+    reached_preemption: Whether the event reached preemption.
+    preemption_received_at: The time when preemption was received.
+    wait_for_prev_start_time: The start time of waiting for previous checkpoint.
+    wait_for_prev_end_time: The end time of waiting for previous checkpoint.
+    checkpoint_start_time: The start time of checkpointing.
+    checkpoint_end_time: The end time of checkpointing.
+    get_old_steps_start_time: The start time of getting old steps.
+    get_old_steps_end_time: The end time of getting old steps.
+    synchronous: Whether the event is synchronous.
+  """
+
+  step: Optional[int] = None
+  event_type: Optional[str] = None
+  start_time: Optional[float] = None
+  end_time: Optional[float] = None
+  reached_preemption: Optional[bool] = False
+  preemption_received_at: Optional[float] = None
+  wait_for_prev_start_time: Optional[float] = None
+  wait_for_prev_end_time: Optional[float] = None
+  checkpoint_start_time: Optional[float] = None
+  checkpoint_end_time: Optional[float] = None
+  get_old_steps_start_time: Optional[float] = None
+  get_old_steps_end_time: Optional[float] = None
+  synchronous: Optional[bool] = False
+
+
+class CheckpointBadputCalculatorTest(absltest.TestCase):
+
+  def setUp(self):
+    """Setup for the test."""
+    super().setUp()
+    mock_gcloud_client = mock.create_autospec(google_cloud_logging.Client)
+    options = checkpoint_breakdown.CheckpointLoggerOptions(
+        job_name=_JOB_NAME,
+        logger_name=_LOGGER_NAME,
+        client=mock_gcloud_client,
+    )
+    self.checkpoint_badput_calculator = (
+        checkpoint_breakdown.CheckpointBadputCalculator(options)
+    )
+    for i in range(1, 5):
+      self.checkpoint_badput_calculator.entries.append(
+          dataclasses.asdict(
+              MockStepStatistics(
+                  step=i,
+                  event_type='save',
+                  start_time=i * 10.0,
+                  wait_for_prev_start_time=i * 10.0,
+                  wait_for_prev_end_time=i * 10.0 + 2,
+                  checkpoint_start_time=i * 10.0 + 2,
+                  checkpoint_end_time=i * 10.0 + 3,
+                  end_time=i * 10.0 + 4,
+                  reached_preemption=False,
+                  preemption_received_at=None,
+                  synchronous=False,
+              )
+          )
+      )
+
+  def test_checkpoint_breakdown(self):
+    total_time = (
+        self.checkpoint_badput_calculator.calculate_blocking_checkpoint_time()
+    )
+    expected_total_time = len(self.checkpoint_badput_calculator.entries)
+    self.assertEqual(total_time, expected_total_time)
+
+  def test_checkpoint_breakdown_with_preemption(self):
+    i = len(self.checkpoint_badput_calculator.entries) + 1
+    preemption_entry = dataclasses.asdict(
+        MockStepStatistics(
+            step=i,
+            event_type='save',
+            start_time=i * 10.0,
+            wait_for_prev_start_time=i * 10.0,
+            wait_for_prev_end_time=i * 10.0 + 2,
+            checkpoint_start_time=i * 10.0 + 2,
+            checkpoint_end_time=i * 10.0 + 3,
+            end_time=i * 10.0 + 4,
+            reached_preemption=True,
+            preemption_received_at=i * 10.0,
+            synchronous=True,
+        )
+    )
+    self.checkpoint_badput_calculator.entries.append(preemption_entry)
+    expected_total_time = (i - 1) + (
+        preemption_entry['wait_for_prev_end_time']
+        - preemption_entry['start_time']
+    )
+    total_time = (
+        self.checkpoint_badput_calculator.calculate_blocking_checkpoint_time()
+    )
+    print(total_time, expected_total_time)
+    self.assertEqual(total_time, expected_total_time)
+
+
+if __name__ == '__main__':
+  absltest.main()
